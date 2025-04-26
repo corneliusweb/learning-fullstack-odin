afaf7705dd8340d3ccc4a5e4438f9eb8045dc28e
Cornelius Asogwa <cornelius.webdev@gmail.com>
2025-04-26 08:18:07 +0100

    Practice inheritance with ChatGPT

--- playground/object-construct.js
+++ playground/object-construct.js
@@ -5,15 +5,15 @@ function Book(title, author, pages, read) {
 	this.read = read;
 }
 
-Book.prototype.info = function() {
+Book.prototype.info = function () {
 	return `${this.title} by ${this.author}, ${this.pages}. ${
 		this.read ? 'Book is read' : 'Book is not yet read'
 	}`;
-}
+};
 
 const book1 = new Book('Learning how to Learn', 'C.C Asogwa', 234, false);
 const book2 = new Book('Learning how to Learn', 'C.C Asogwa', 234, true);
-console.log(book1.info());
+// console.log(book1.info());
 
 // legacy way
 const obj = {
@@ -26,7 +26,7 @@ const obj = {
 	},
 };
 
-console.log(obj.info());
+// console.log(obj.info());
 
 function Player(name, marker) {
 	if (!new.target) {
@@ -38,8 +38,58 @@ function Player(name, marker) {
 
 const player1 = new Player('Jenn', 'X');
 const player2 = new Player('Tim', 'O');
-console.log(player2);
+// console.log(player2);
 
 // console.log(Object.getPrototypeOf(Player.prototype) === Object.prototype);
 // console.log(Object.prototype.hasOwnProperty('valueOf'));
 // console.log(Object.getPrototypeOf(player1) === Player.prototype);
+
+// chatGPT hands-on exercise: Family Inheritance
+
+//WITH PLAIN OBJECT
+const grandDad = { land: true };
+
+let dad = Object.create(grandDad);
+dad.house = true;
+
+let me = Object.create(dad);
+me.car = true;
+me.wife = true;
+
+let myChild = Object.create(me);
+
+grandDad.getHeritage = function () {
+	return this.land;
+};
+
+// console.log(myChild.getHeritage());
+
+function Hero(name, level) {
+	this.name = name;
+	this.level = level;
+}
+
+function Warrior(name, level, weapon) {
+	Hero.call(this, name, level);
+	this.weapon = weapon;
+}
+
+function Healer(name, level, spell) {
+	Hero.call(this, name, level);
+	this.spell = spell;
+}
+
+Object.setPrototypeOf(Warrior.prototype, Hero.prototype);
+Object.setPrototypeOf(Healer.prototype, Hero.prototype)
+
+Warrior.prototype.attack = function () {
+	return `${this.name} attacks with ${this.weapon}`;
+};
+
+Hero.prototype.greet = function () {
+	return `${this.name} greets you!`;
+};
+
+const hero1 = new Warrior('jammy', 34, 'brain');
+
+console.log(hero1.greet());
