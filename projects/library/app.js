7ba0c2006f61a260b959219300498f8523d2be1c
Cornelius Asogwa <cornelius.webdev@gmail.com>
2025-05-08 09:23:21 +0100

    Write the displayBook fn, and the ui structure

--- projects/library/app.js
+++ projects/library/app.js
@@ -1,4 +1,4 @@
-let library = [];
+let myLibrary = [];
 
 function Book(title, author, pages, pub, year, id) {
 	this.title = title;
@@ -22,5 +22,51 @@ function addBookToLibrary(title, author, pages, pub, year) {
 	const id = crypto.randomUUID(); // generate unique id
 	const newBook = new Book(title, author, pages, pub, year, id);
 
-	library.push(newBook);
+	myLibrary.push(newBook);
 }
+
+const displayDiv = document.querySelector('.display');
+const ol = document.querySelector('.display ol');
+
+function displayBook(id) {
+	const bookList = document.createElement('li');
+	const bookInfoPara = document.createElement('p');
+	bookInfoPara.innerHTML = `<span>${title.value}</span> ${author.value}, ${pages.value}, ${pub.value} ${year.value}.`;
+
+	const deleteBtn = document.createElement('button');
+	deleteBtn.textContent = 'x';
+
+	// create deleteBtn attributes
+	const delBtnAttrs = {
+		'data-id': id,
+		class: 'del-btn',
+	};
+	Object.entries(delBtnAttrs).forEach(([key, value]) => {
+		deleteBtn.setAttribute(key, value);
+	});
+
+	// create read status elems
+	const readStatusPara = document.createElement('p');
+	const readStatusBtn = document.createElement('button');
+	readStatusBtn.textContent = 'Mark read';
+
+	// create para and btn divs to group them for flex
+	const paraDiv = document.createElement('div');
+	const btnDiv = document.createElement('div');
+
+	paraDiv.appendChild(bookInfoPara);
+	paraDiv.appendChild(readStatusPara);
+
+	btnDiv.appendChild(deleteBtn);
+	btnDiv.appendChild(readStatusBtn);
+
+	bookList.appendChild(paraDiv);
+	bookList.appendChild(btnDiv);
+}
+
+// get modal elems
+
+// const modal = document.querySelector('.modal');
+
+// const newBookBtn = document.querySelector('.new-book-btn');
+// newBookBtn.addEventListener('click', () => (modal.style.display = 'block'));
